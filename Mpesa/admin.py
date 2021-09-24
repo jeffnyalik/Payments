ed4002300dd08a2c7f5f0d8875618f7c7c31db41
Jeff Nyalik <jeffnyak@gmail.com>
2021-09-24 21:01:25 +0300

    commits

--- Mpesa/admin.py
+++ Mpesa/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import BaseModel, PaymentTransaction, Wallet
+from .models import BaseModel, PaymentTransaction, Wallet, PayBillPayment
 
 
 admin.site.register(PaymentTransaction)
 admin.site.register(Wallet)
+admin.site.register(PayBillPayment)
